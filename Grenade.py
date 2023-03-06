bd85785cbc6e6f419e1a4b1b345c381c85fe1b4e
Tobias <tobiaskrogshede@gmail.com>
2023-03-06 10:07:12 +0100

    gg
    m

--- Grenade.py
+++ Grenade.py
@@ -5,7 +5,7 @@ class Grenade(GameObject):
     def __init__(self, x, y) -> None:
         super().__init__()
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("grenade.png").convert_alpha()
+        self.image = pygame.image.load("Grenade.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.move_ip(x, y)
 
