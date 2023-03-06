1f35e2b439fb18e63aa1ebc22f8f11ed0e644173
Intermetheus <93767470+Intermetheus@users.noreply.github.com>
2023-03-06 12:35:34 +0100

    Environment

--- Environment.py
+++ Environment.py
@@ -5,7 +5,8 @@ class Environment(GameObject):
     def __init__(self, name, x, y) -> None:
         super().__init__()
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(name + ".png").convert_alpha()       
+        self.image = pygame.image.load(name + ".png").convert_alpha()   
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * 10, self.image.get_height() * 10))    
         self.rect = self.image.get_rect()
         self.rect.move_ip(x, y)
 
